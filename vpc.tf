6fd45d72ae35aedd718d92561a04321cf71c5e9d
Cvaaaa <118925407+Cvaaaa@users.noreply.github.com>
2023-03-06 17:01:52 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,21 +7,3 @@ resource "aws_vpc" "main" {
     Purpose = "Jenkins Demo"
   }
 }
-
-resource "aws_subnet" "t-sub1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.0.0/24"
-
-  tags = {
-    Name = "public"
-  }
-}
-
-resource "aws_subnet" "t-sub2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.1.0/24"
-
-  tags = {
-    Name = "private"
-  }
-}
