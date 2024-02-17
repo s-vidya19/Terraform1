c3d73433b149b671fe344ddf67a9764f6d9958e5
s-vidya19 <151515106+s-vidya19@users.noreply.github.com>
2024-02-17 16:08:00 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -55,7 +55,7 @@ resource "aws_internet_gateway" "first_ig" {
 }
 
 resource "aws_instance" "connect" {
-  ami           = "ami-014d05e6b24240371"
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.sub1.id
   key_name   = "jen-k"
